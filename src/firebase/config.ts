50204a825cfa8a6ce6662e41495f2415e3fd6d7f
Mustafa Uslu <paqronnetwork@gmail.com>
2025-12-01 02:06:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8731896430-bb006",
+  "appId": "1:1079637899062:web:1104c540ae6106b503a597",
+  "apiKey": "AIzaSyBTSsE6K3TqUpxh6iGL5BZFREiuf5gKSZo",
+  "authDomain": "studio-8731896430-bb006.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1079637899062"
+};
