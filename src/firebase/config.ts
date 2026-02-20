45124a04da7bc5f203b8f627bf4c07ffa636ebca
Mustafa USLU | 1Protocol <68284874+1protocol@users.noreply.github.com>
2026-02-20 19:43:03 +0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-8731896430-bb006",
-  "appId": "1:1079637899062:web:1104c540ae6106b503a597",
-  "apiKey": "AIzaSyBTSsE6K3TqUpxh6iGL5BZFREiuf5gKSZo",
+  "appId": "ıd-buraya",
+  "apiKey": "API-BURAYA",
   "authDomain": "studio-8731896430-bb006.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1079637899062"
